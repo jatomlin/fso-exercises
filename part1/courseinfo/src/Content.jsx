2f0b7c287e099a7ba109b8de9fa69f1d90888e2a
jatomlin <justin.a.tomlin@gmail.com>
2025-12-08 22:24:10 -0800

    Catch up to Exercise 1.5 refactoring of courseinfo. Remove array iterating for now

--- part1/courseinfo/src/Content.jsx
+++ part1/courseinfo/src/Content.jsx
@@ -3,7 +3,10 @@ import Part from './Part.jsx'
 const Content = (props) => {
   return (
     <div>
-      {props.parts.map((part) => (<Part part={part} />))}
+      {/*props.parts.map((part) => (<Part part={part} />))*/}
+      <Part part={props.parts[0]} />
+      <Part part={props.parts[1]} />
+      <Part part={props.parts[2]} />
     </div>
   )
 }
--- part1/courseinfo/src/Total.jsx
+++ part1/courseinfo/src/Total.jsx
@@ -1,10 +1,16 @@
 const Total = (props) => {
+  /*
   const sum = props.parts.reduce(
-    (accumulator, currentValue) => 
-        accumulator + currentValue.exercises, 0
-);
+  (accumulator, currentValue) => 
+    accumulator + currentValue.exercises, 0
+  );
+  */
   return (
-    <p>Number of exercises {sum}</p>
+    <p>
+        Number of exercises {props.parts[0].exercises 
+                            + props.parts[1].exercises 
+                            + props.parts[2].exercises}
+    </p>
   )
 }
 
